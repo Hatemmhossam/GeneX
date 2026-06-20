af8ee919a2e05222e6ea9b8dfd78a0ec0c128821
Hatemmhossam <168200109+Hatemmhossam@users.noreply.github.com>
2026-06-20 21:14:15 +0300

    Load Firebase credentials from environment

--- genex_project/genex_Backend/api/firebase_admin.py
+++ genex_project/genex_Backend/api/firebase_admin.py
@@ -1,9 +1,16 @@
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials
-from django.conf import settings
 
 if not firebase_admin._apps:
-    cred_path = os.path.join(settings.BASE_DIR, "firebase-admin-sdk.json")
-    cred = credentials.Certificate(cred_path)
+    firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
+
+    if firebase_json:
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
+    else:
+        cred_path = os.path.join(os.getcwd(), "firebase-admin-sdk.json")
+        cred = credentials.Certificate(cred_path)
+
     firebase_admin.initialize_app(cred)
